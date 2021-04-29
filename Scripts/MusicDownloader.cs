9d3a9d9ec553422370db20770e6b2821c3f733d1
FaStNiNjAzZ <47434948+FaStNiNjAzZ@users.noreply.github.com>
2021-04-29 01:45:38 +0100

    Uploaded Source

---
+++ Scripts/MusicDownloader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicDownloader : MonoBehaviour
+{
+    public InputField songIDInputField;
+    public Text downloadStatusText;
+
+    string[] Songs;
+
+    public string songID;
+    public string finalURL;
+
+    bool fileExisting = false;
+
+    string filePath;
+
+    public string url;
+
+    //Downloads the song with Newgrounds
+    public void DownloadSong()
+    {
+        SongExistCheck();
+
+        //C:\Users\{NAME}\AppData\LocalLow\DefaultCompany\{ProjectName}
+        string path = Application.persistentDataPath;
+
+        //URL for host website
+        url = "https://www.newgrounds.com/audio/download/";
+
+        //Takes song ID from an input field
+        songID = songIDInputField.text;
+
+        //Combines the URL and song ID together in 1 string
+        finalURL = (url + songID);
+
+        WebClient client = new WebClient();
+
+        //Downloads the file using the URL (finalURL). @path is where the file is downloading to. The rest is what the file is called, in this case it's /{songID}.mp3
+        client.DownloadFile(finalURL, @path + "/" + songID + ".mp3");
+
+        //Gets the MP3 file
+        Songs = Directory.GetFiles(Application.persistentDataPath, ".mp3");
+        
+        //Gets the file path and is stored in filePath
+        filePath = Application.persistentDataPath + "  " + Songs;
+
+        SongExistCheck();
+    }
+
+    //Looks to see if the download was successful and outputs it to a text field
+    public void SongExistCheck()
+    {
+        fileExisting = false;
+        songID = songIDInputField.text;
+        string path = Application.persistentDataPath;
+        string curFile = (@path + "/" + songID + ".mp3");
+        Debug.Log(curFile);
+        Debug.Log(File.Exists(curFile) ? fileExisting = true : fileExisting = false);
+
+        if (fileExisting == true)
+        {
+            downloadStatusText.text = ("Download Success!");
+            downloadStatusText.color = new Color(0.2f, 1f, 0.2f, 1f);
+        }
+
+        else if (fileExisting == false)
+        {
+            downloadStatusText.text = ("Download Failed!");
+            downloadStatusText.color = new Color(1f, 0.2f, 0.2f, 1f);
+        }
+    }
+}
---
+++ Scripts/MusicPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MusicPlayer : MonoBehaviour
+{
+    //Directory of folder to be searched anywhere on the computer
+    public string FileDirectory;
+
+    //Audio source
+    public AudioSource Source;
+    //Current sound playing
+    public AudioClip Clip;
+
+    //List of all valid directories
+    List<string> Files = new List<string>();
+    //List of all AudioClips
+    List<AudioClip> Clips = new List<AudioClip>();
+
+    string[] files;
+
+    public void Start()
+    {
+        //Checks for existing files
+        CheckFile();
+    }
+
+    public void PlaySong(int _listIndex)
+    {
+        //Checks if the song is playing
+        if (!Source.isPlaying)
+        {
+            Clip = Clips[_listIndex];
+            Source.clip = Clip;
+            Source.Play();
+            CheckFile();
+        }
+    }
+
+    void Update()
+    {
+        //Selects the songs randomly
+        var musicSelector = UnityEngine.Random.Range(0, files.Length);
+
+        //Plays song with whatever number musicSelector has
+        PlaySong(musicSelector);
+    }
+
+    public void CheckFile()
+    {
+        //File Location for .MP3
+        FileDirectory = Application.persistentDataPath;
+
+        //Grabs all files from FileDirectory
+        files = Directory.GetFiles(FileDirectory);
+
+        //Checks all files and stores all WAV files into the Files list.
+        for (int i = 0; i < files.Length; i++)
+        {
+            if (files[i].EndsWith(".mp3"))
+            {
+                Files.Add(files[i]);
+                Clips.Add(new WWW(files[i]).GetAudioClip(false, true, AudioType.MPEG));
+            }
+        }
+    }
+}
